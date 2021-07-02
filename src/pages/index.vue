4b70db3c92b77b8f2e2f4d32c5ab25b1feeb3558
mzaini30 <muhzaini30@gmail.com>
2021-07-03 00:16:40 +0800

    upload

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,4 +2,18 @@
 	<teleport to='head'>
 		<title>Cek Harga</title>
 	</teleport>
+
+	<div class="p-2 bg-pink-400 shadow text-white">Cek Harga</div>
+	<div class="">
+		<div class="" v-for='x in data'>
+			
+		</div>
+	</div>
 </template>
+
+<script setup>
+	import {ref} from 'vue'
+	const data = ref([
+		{namaBarang: '', harga: '', kuantitas: '', hasilBagi: ''}
+	])
+</script>
