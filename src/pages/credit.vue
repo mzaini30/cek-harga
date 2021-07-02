bc4c275e550698b1fabc1293b590fe1ceb929654
mzaini30 <muhzaini30@gmail.com>
2021-07-03 00:20:23 +0800

    upload

---
+++ src/pages/credit.vue
@@ -0,0 +1,7 @@
+<template>
+	<div>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+
+	<teleport to='head'>
+		<title>Credit</title>
+	</teleport>
+</template>
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,19 @@
 function index() {
   return import(/* webpackChunkName: "index" */ '/src/pages/index.vue')
 }
+function credit() {
+  return import(/* webpackChunkName: "credit" */ '/src/pages/credit.vue')
+}
 
 export default [
   {
     name: 'index',
     path: '/',
     component: index,
+  },
+  {
+    name: 'credit',
+    path: '/credit',
+    component: credit,
   },
 ]
