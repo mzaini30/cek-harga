a13dfdea79a96cbe61aa77d7e634680eb35fcaf2
mzaini30 <muhzaini30@gmail.com>
2021-07-03 00:29:37 +0800

    upload

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,17 +3,31 @@
 		<title>Cek Harga</title>
 	</teleport>
 
-	<div class="p-2 bg-pink-400 shadow text-white">Cek Harga</div>
-	<div class="">
-		<div class="" v-for='x in data'>
-			
+	<div class="p-2 bg-red-400 shadow text-white">Cek Harga</div>
+	<div class="p-2">
+		<div class="border mb-2 bg-red-300 shadow rounded p-2" v-for='x in data'>
+			<div class="pb-2 grid grid-cols-6">
+				<input type="text" class='inputan col-span-4' placeholder="nama barang">
+				<input type="text" class='inputan col-span-2 bg-purple-200' placeholder="hasil" readonly>
+			</div>
+			<div class="grid grid-cols-2">
+				<input type="text" class="inputan" placeholder="harga">
+				<input type="text" class="inputan" placeholder="banyak">
+			</div>
 		</div>
 	</div>
 </template>
 
+<style scoped>
+	.inputan {
+		@apply focus:outline-none border p-1 w-full;
+	}
+</style>
+
 <script setup>
 	import {ref} from 'vue'
 	const data = ref([
-		{namaBarang: '', harga: '', kuantitas: '', hasilBagi: ''}
+		{namaBarang: '', harga: '', kuantitas: '', hasilBagi: ''},
+		{namaBarang: '', harga: '', kuantitas: '', hasilBagi: ''},
 	])
 </script>
